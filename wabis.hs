98c4a9a86645dd91a13ae446dc21a0a946ef3c98
Oz <oz@petantik.net>
2013-02-26 16:37:06 +0000

    added item positioning finding, and other item types in the world

--- wabis.hs
+++ wabis.hs
@@ -8,17 +8,28 @@ type WorldMap = [String]
 
 thisWorld :: WorldMap
 thisWorld =  ["........"
-             ,"......--"
-             ,"--......"
-             ,"....----"
-             ,"........"]
+             ,"...*..--"
+             ,"--....?."
+             ,"!...#---"
+             ,"..@....."]
 
 
 type Pos = (Int, Int) 
 
-data WorldItems = Clear | Wall | Foe | Unknown deriving Show
+data WorldItems = Clear
+                | Wall
+                | Friend
+                | Foe
+                | Neutral
+                | Mystery
+                | Player
+                | Unknown deriving (Show, Eq)
 
 type ItemPos = (WorldItems, Pos)
+
+worldW = length (head thisWorld)
+worldH = length thisWorld
+worldCurrent = worldToItemPos thisWorld
 
 worldToItemPos :: WorldMap -> [ItemPos]
 worldToItemPos  = liftM2 zip (concat . worldToItems) (concat . worldToPos) 
@@ -39,14 +50,27 @@ makeIndexedArray (x,y) = zipWith indexHelper ys xs
 indexHelper :: Int -> [Int] -> [Pos]
 indexHelper y  = map (\x -> (x,y)) 
 
+findItem :: WorldItems -> [ItemPos]-> [Pos]
+findItem _ [] = []
+findItem i (ip:ips)
+            | fst ip == i = snd ip: findItem i ips
+            | otherwise   = findItem i ips
+
+
 charToItem :: Char -> WorldItems
 charToItem c = case c of 
                     '.' -> Clear
                     '-' -> Wall
-                    '#' -> Foe
+                    '#' -> Friend
+                    '!' -> Foe
+                    '*' -> Neutral
+                    '?' -> Mystery
+                    '@' -> Player
                     _ -> Unknown
 
 main :: IO ()
 main = do
+    putStrLn "Hello, Player"
     putStrLn "Enter Game"
+
     
